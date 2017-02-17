b273510ff1cc1c2c9c9a5f90acb9b4e66d461a5f
Krzysztof Chmielewski <chmielewski.k.s@gmail.com>
2017-02-17 20:49:56 +0100

    Fixed naming and 3rd module

---
+++ 3 Java configuration/src/main/java/io/kch/spring/mvc3/configuration/HelloWorldConfiguration.java
@@ -0,0 +1,26 @@
+package io.kch.spring.mvc3.configuration;
+
+import io.kch.spring.mvc3.controller.HelloWorldController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackageClasses = HelloWorldController.class)
+public class HelloWorldConfiguration {
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+
+        return viewResolver;
+    }
+
+}
